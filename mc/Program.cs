1aa1fdd1c0e6c4a0d10a2cddf42c3077cd7ee94d
MICHAEL WELSH <mikewelsh801@gmail.com>
2023-05-23 16:56:12 -0600

    started working on parser

--- mc/Program.cs
+++ mc/Program.cs
@@ -1,2 +1,185 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿while (true)
+{
+    Console.Write("> ");
+    var line = Console.ReadLine();
+    if (string.IsNullOrEmpty(line))
+        return;
+
+    var lexer = new Lexer(line);
+    while (true)
+    {
+        var token = lexer.NextToken();
+        if (token.Kind == SyntaxKind.EndOfFileToken)
+            break;
+
+        Console.Write($"{token.Kind}: '{token.Text}'");
+        if (token.Value != null)
+            Console.Write($" {token.Value}");
+
+        Console.WriteLine();
+    }
+}
+
+enum SyntaxKind
+{
+    NumberToken,
+    WhitespaceToken,
+    PlusToken,
+    MinusToken,
+    StarToken,
+    SlashToken,
+    OpenParenToken,
+    CloseParenToken,
+    BadToken,
+    EndOfFileToken,
+    NumberExpression
+}
+
+class SyntaxToken
+{
+    public SyntaxKind Kind { get; }
+    public int Position { get; }
+    public string Text { get; }
+    public object? Value { get; }
+
+    public SyntaxToken(SyntaxKind kind, int position, string text, object? value)
+    {
+        Kind = kind;
+        Position = position;
+        Text = text;
+        Value = value;
+    }
+}
+
+class Lexer
+{
+    private readonly string _text;
+    private int _position;
+
+    public Lexer(string text)
+    {
+        _text = text;
+    }
+
+    private char Current
+    {
+        get
+        {
+            if (_position >= _text.Length)
+                return '\0';
+            return _text[_position];
+        }
+    }
+
+    private void Next()
+    {
+        _position++;
+    }
+
+    public SyntaxToken NextToken()
+    {
+        // <numbers>
+        // + - * / ()
+        // <whitespace>
+
+        if (_position >= _text.Length)
+            return new SyntaxToken(SyntaxKind.EndOfFileToken, _position, "\0", null);
+
+        if (char.IsDigit(Current))
+        {
+            var start = _position;
+
+            while (char.IsDigit(Current))
+                Next();
+
+            var length = _position - start;
+            var text = _text.Substring(start, length);
+            int.TryParse(text, out var value);
+            return new SyntaxToken(SyntaxKind.NumberToken, start, text, value);
+        }
+
+        if (char.IsWhiteSpace(Current))
+        {
+            var start = _position;
+
+            while (char.IsWhiteSpace(Current))
+                Next();
+
+            var length = _position - start;
+            var text = _text.Substring(start, length);
+            return new SyntaxToken(SyntaxKind.WhitespaceToken, start, text, null);
+        }
+
+        if (Current == '+')
+            return new SyntaxToken(SyntaxKind.PlusToken, _position++, "+", null);
+        else if (Current == '-')
+            return new SyntaxToken(SyntaxKind.MinusToken, _position++, "-", null);
+        else if (Current == '*')
+            return new SyntaxToken(SyntaxKind.StarToken, _position++, "*", null);
+        else if (Current == '/')
+            return new SyntaxToken(SyntaxKind.SlashToken, _position++, "/", null);
+        else if (Current == '(')
+            return new SyntaxToken(SyntaxKind.OpenParenToken, _position++, "(", null);
+        else if (Current == ')')
+            return new SyntaxToken(SyntaxKind.CloseParenToken, _position++, ")", null);
+
+        return new SyntaxToken(SyntaxKind.BadToken, _position++, _text.Substring(_position - 1, 1), null);
+    }
+}
+
+abstract class SyntaxNode
+{
+    public abstract SyntaxKind Kind { get; }
+}
+
+abstract class ExpressionSyntax : SyntaxNode
+{
+}
+
+sealed class NumberSyntax : ExpressionSyntax
+{
+    public override SyntaxKind Kind => SyntaxKind.NumberExpression;
+
+    public NumberSyntax(SyntaxToken numberToken)
+    {
+
+    }
+}
+
+class Parser
+{
+    private readonly SyntaxToken[] _tokens;
+    private int _position;
+
+    public Parser(string text)
+    {
+        List<SyntaxToken> tokens = new();
+        Lexer lexer = new(text);
+
+        SyntaxToken token;
+        do
+        {
+            token = lexer.NextToken();
+
+            if (token.Kind != SyntaxKind.WhitespaceToken &&
+               token.Kind != SyntaxKind.BadToken)
+            {
+                tokens.Add(token);
+            }
+
+        } while (token.Kind != SyntaxKind.EndOfFileToken);
+
+        _tokens = tokens.ToArray();
+    }
+
+    private SyntaxToken Peek(int offset)
+    {
+        var index = _position + offset;
+        if (index >= _tokens.Length)
+            return _tokens[_tokens.Length - 1];
+
+        return _tokens[index];
+    }
+
+    private SyntaxToken Current => Peek(0);
+}
