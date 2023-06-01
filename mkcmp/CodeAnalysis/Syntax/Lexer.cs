0067ef9c6df4513456d4f344d2dd4e6ae9df5e80
MICHAEL WELSH <mikewelsh801@gmail.com>
2023-06-01 09:25:56 -0600

    Added better diagnostic reporting. Fixed error in lexer that broke errors.

--- mkcmp/CodeAnalysis/Syntax/Lexer.cs
+++ mkcmp/CodeAnalysis/Syntax/Lexer.cs
@@ -34,9 +34,10 @@ namespace Mkcmp.CodeAnalysis.Syntax
             if (_position >= _text.Length)
                 return new SyntaxToken(SyntaxKind.EndOfFileToken, _position, "\0", null);
 
+            var start = _position;
+
             if (char.IsDigit(Current))
             {
-                var start = _position;
 
                 while (char.IsDigit(Current))
                     Next();
@@ -51,8 +52,6 @@ namespace Mkcmp.CodeAnalysis.Syntax
 
             if (char.IsWhiteSpace(Current))
             {
-                var start = _position;
-
                 while (char.IsWhiteSpace(Current))
                     Next();
 
@@ -63,8 +62,6 @@ namespace Mkcmp.CodeAnalysis.Syntax
 
             if (char.IsLetter(Current))
             {
-                var start = _position;
-
                 while (char.IsLetter(Current))
                     Next();
 
@@ -90,21 +87,36 @@ namespace Mkcmp.CodeAnalysis.Syntax
                     return new SyntaxToken(SyntaxKind.CloseParenToken, _position++, ")", null);
                 case '&':
                     if (Lookahead == '&')
-                        return new SyntaxToken(SyntaxKind.AmpersandAmpersandToken, _position += 2, "&&", null);
+                    {
+                        _position += 2;
+                        return new SyntaxToken(SyntaxKind.AmpersandAmpersandToken, start, "&&", null);
+                    }
                     break;
                 case '|':
                     if (Lookahead == '|')
-                        return new SyntaxToken(SyntaxKind.PipePipeToken, _position += 2, "||", null);
+                    {
+                        _position += 2;
+                        return new SyntaxToken(SyntaxKind.PipePipeToken, start, "||", null);
+                    }
                     break;
                 case '=':
                     if (Lookahead == '=')
-                        return new SyntaxToken(SyntaxKind.EqualsEqualsToken, _position += 2, "==", null);
+                    {
+                        _position += 2;
+                        return new SyntaxToken(SyntaxKind.EqualsEqualsToken, start, "==", null);
+                    }
                     break;
                 case '!':
                     if (Lookahead == '=')
-                        return new SyntaxToken(SyntaxKind.BangEqualsToken, _position += 2, "!=", null);
+                    {
+                        _position += 2;
+                        return new SyntaxToken(SyntaxKind.BangEqualsToken, start, "!=", null);
+                    }
                     else
-                        return new SyntaxToken(SyntaxKind.BangToken, _position++, "!", null);
+                    {
+                        _position++;
+                        return new SyntaxToken(SyntaxKind.BangToken, start, "!", null);
+                    }
             }
 
             _diagnostics.ReportBadCharacter(_position, Current);
