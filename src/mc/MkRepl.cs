c2821202ef493922d932df22789b9ca2adfb1c40
MICHAEL WELSH <mikewelsh801@gmail.com>
2023-07-05 14:46:00 -0600

    Moved GetLastToken to SyntaxNode.

--- src/mc/MkRepl.cs
+++ src/mc/MkRepl.cs
@@ -98,18 +98,10 @@ internal sealed class MkRepl : Repl
             return true;
 
         var syntaxTree = SyntaxTree.Parse(text);
-        if (GetLastToken(syntaxTree.Root.Statement).IsMissing)
+        if (syntaxTree.Root.Statement.GetLastToken().IsMissing)
             return false;
 
         return true;
-    }
-
-    private static SyntaxToken GetLastToken(SyntaxNode node)
-    {
-        if (node is SyntaxToken token)
-            return token;
-
-        return GetLastToken(node.GetChildren().Last());
     }
 
     protected override void EvaluateSubmission(string text)
