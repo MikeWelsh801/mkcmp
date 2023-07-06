a7abaf24efa2e4fa2b072e2d5adfbb28f8bef985
MICHAEL WELSH <mikewelsh801@gmail.com>
2023-07-06 15:45:43 -0600

    added tag to isReadOnly call for clarity

--- src/mkcmp/CodeAnalysis/Binding/Binder.cs
+++ src/mkcmp/CodeAnalysis/Binding/Binder.cs
@@ -122,7 +122,7 @@ internal sealed class Binder
 
         _scope = new BoundScope(_scope);
 
-        var variable = BindVariable(syntax.Identifier, true, TypeSymbol.Int);
+        var variable = BindVariable(syntax.Identifier, isReadOnly: true, TypeSymbol.Int);
         var body = BindStatement(syntax.Body);
 
         _scope = _scope.Parent;
