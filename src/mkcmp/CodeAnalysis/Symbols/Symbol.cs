05f5fe0eabce391b4a6aaa805cebb6346ac2f3b5
MICHAEL WELSH <mikewelsh801@gmail.com>
2023-07-06 15:50:28 -0600

    refactor

--- src/mkcmp/CodeAnalysis/Symbols/Symbol.cs
+++ src/mkcmp/CodeAnalysis/Symbols/Symbol.cs
@@ -2,7 +2,7 @@ namespace Mkcmp.CodeAnalysis.Symbols;
 
 public abstract class Symbol
 {
-    internal Symbol(string name)
+    private protected Symbol(string name)
     {
         Name = name;
     }
