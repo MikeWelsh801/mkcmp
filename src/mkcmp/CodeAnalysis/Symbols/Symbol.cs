e29d47238f76fd460b78cfc0ae826df4e75d5934
MICHAEL WELSH <mikewelsh801@gmail.com>
2023-07-06 12:47:31 -0600

    Added abstract symbol type.

---
+++ src/mkcmp/CodeAnalysis/Symbols/Symbol.cs
@@ -0,0 +1,13 @@
+namespace Mkcmp.CodeAnalysis.Symbols;
+
+public abstract class Symbol
+{
+    protected Symbol(string name)
+    {
+        Name = name;
+    }
+
+    public abstract SymbolKind Kind { get; }
+    public string Name { get; }
+}
+
---
+++ src/mkcmp/CodeAnalysis/Symbols/SymbolKind.cs
@@ -0,0 +1,7 @@
+namespace Mkcmp.CodeAnalysis.Symbols;
+
+public enum SymbolKind
+{
+    Variable,
+}
+
