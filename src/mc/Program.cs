37904c156834cf5f8235c99d1d6c99679fe17405
MICHAEL WELSH <mikewelsh801@gmail.com>
2023-08-09 17:32:44 -0600

    move diagnostic writing to text writer extensions, so compiler has access to them

--- src/mc/Program.cs
+++ src/mc/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace mc;
+﻿using Mkcmp.CodeAnalysis;
+using Mkcmp.CodeAnalysis.Symbols;
+using Mkcmp.CodeAnalysis.Syntax;
+
+namespace Mkcmp;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        if (args.Length == 0)
+        {
+            Console.Error.WriteLine("usage: mc <source-path>");
+            return;
+        }
+
+        if (args.Length > 1)
+        {
+            Console.Error.WriteLine("error: only one path supported right now");
+            return;
+        }
+        var path = args.Single();
+
+        var text = File.ReadAllText(path);
+        var _syntaxTree = SyntaxTree.Parse(text);
+        var compilation = new Compilation(_syntaxTree);
+        compilation.Evaluate(new Dictionary<VariableSymbol, object>());
     }
 }
--- src/mkcmp/IO/TextWriterExtensions.cs
+++ src/mkcmp/IO/TextWriterExtensions.cs
@@ -1,16 +1,18 @@
 using System.CodeDom.Compiler;
+using Mkcmp.CodeAnalysis;
 using Mkcmp.CodeAnalysis.Syntax;
+using Mkcmp.CodeAnalysis.Text;
 
 namespace Mkcmp.IO;
 
-internal static class TextWriterExtensions
+public static class TextWriterExtensions
 {
 
     private static bool IsConsoleOut(this TextWriter writer)
     {
         if (writer == Console.Out)
             return true;
-        
+
         if (writer is IndentedTextWriter iw && iw.InnerWriter.IsConsoleOut())
             return true;
 
@@ -114,5 +116,44 @@ internal static class TextWriterExtensions
         writer.SetForeground(ConsoleColor.DarkGray);
         writer.Write(text);
         writer.ResetColor();
+    }
+
+    public static void WriteDiagnostics(this TextWriter writer, IEnumerable<Diagnostic> diagnostics, SyntaxTree syntaxTree)
+    {
+        foreach (var diagnostic in diagnostics.OrderBy(d => d.Span.Start)
+                                              .ThenBy(d => d.Span.Length))
+        {
+            var lineIndex = syntaxTree.Text.GetLineIndex(diagnostic.Span.Start);
+            var lineNumber = lineIndex + 1;
+            var line = syntaxTree.Text.Lines[lineIndex];
+            var character = diagnostic.Span.Start - line.Start + 1;
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write($"({lineNumber}, {character}): ");
+            Console.WriteLine(diagnostic);
+            Console.ResetColor();
+
+            var prefixSpan = TextSpan.FromBounds(line.Start, diagnostic.Span.Start);
+            var suffixSpan = TextSpan.FromBounds(diagnostic.Span.End, line.End);
+
+            var prefix = syntaxTree.Text.ToString(prefixSpan);
+            var error = syntaxTree.Text.ToString(diagnostic.Span);
+            var suffix = syntaxTree.Text.ToString(suffixSpan);
+
+            Console.Write("    ");
+            Console.Write(prefix);
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write(error);
+            Console.ResetColor();
+
+            Console.Write(suffix);
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
     }
 }
