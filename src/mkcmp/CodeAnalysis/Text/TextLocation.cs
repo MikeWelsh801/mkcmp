7eeb079aac00693bbf605e2a4f9c8e5d772fc2df
MICHAEL WELSH <mikewelsh801@gmail.com>
2023-08-15 12:16:37 -0600

    fix error in text location

--- src/mkcmp/CodeAnalysis/Text/TextLocation.cs
+++ src/mkcmp/CodeAnalysis/Text/TextLocation.cs
@@ -15,6 +15,6 @@ public struct TextLocation
     public int StartLine => Text.GetLineIndex(Span.Start);
     public int StartCharacter => Span.Start - Text.Lines[StartLine].Start;
     public int EndLine => Text.GetLineIndex(Span.End);
-    public int EndCharacter => Span.End - Text.Lines[StartLine].Start;
+    public int EndCharacter => Span.End - Text.Lines[EndLine].Start;
 }
 
